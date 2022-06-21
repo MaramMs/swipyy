2dd00faef65cf0c5251d17f540c4731a84d81dfd
Maran.I.Msabeh <maramismail2015@gmail.com>
2022-06-21 18:44:49 +0300

    create modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
