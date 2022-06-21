a99b3cb699be36942462a778db35cb4e81fc921f
Maran.I.Msabeh <maramismail2015@gmail.com>
2022-06-21 12:26:09 +0300

    create signup-page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { SignUp,SignIn } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-        
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
